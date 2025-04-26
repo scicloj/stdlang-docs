83c9a26964859dfefa934556eb7e1e42a10a925a
Chris Zheng <chris@statstrade.io>
2025-04-26 18:54:19 +1000

    adding stuff

--- notebooks/stdlang_book/walkthrough.clj
+++ notebooks/stdlang_book/walkthrough.clj
@@ -105,7 +105,7 @@
 
 
 
-;; # Primitives
+;; # Types - Primitives
 ;;
 ;; The seven primitive data types in JavaScript are string, number, bigint, boolean, undefined, symbol, and null.
 ;; We work with examples from: https://www.w3docs.com/learn-javascript/methods-of-primitives.html
@@ -129,17 +129,7 @@
 (display-output-format
  ['undefined
   (l/! [:code] undefined)
-  (l/! [:live] undefined)]
- ['NaN
-  (l/! [:code] NaN)
-  (l/! [:live] NaN)]
- ['Infinity
-  (l/! [:code] Infinity)
-  (l/! [:live] Infinity)]
- ['(- Infinity)
-  (l/! [:code] (- Infinity))
-  (l/! [:live] (. (- Infinity)
-                  (toString)))])
+  (l/! [:live] undefined)])
 
 ;; ### Boolean
 
@@ -173,7 +163,17 @@
   (l/! [:live] 1.5)]
  ['1.54444444444444
   (l/! [:code] 1.54444444444444)
-  (l/! [:live] 1.54444444444444)])
+  (l/! [:live] 1.54444444444444)]
+ ['NaN
+  (l/! [:code] NaN)
+  (l/! [:live] NaN)]
+ ['Infinity
+  (l/! [:code] Infinity)
+  (l/! [:live] Infinity)]
+ ['(- Infinity)
+  (l/! [:code] (- Infinity))
+  (l/! [:live] (. (- Infinity)
+                  (toString)))])
 
 ;; ### Bigint
 
@@ -195,12 +195,138 @@
   (l/! [:live] (. (Symbol "hello")
                   (toString)))])
 
+;; # Types - Additional
+
 ;; ### Regex
 
 (display-output-format
  [#"^[Hh]ello d$"
   (l/! [:code] #"^[Hh]ello d$")
   (l/! [:live] #"^[Hh]ello d$")])
+
+
+;; # Types - Collection
+
+;; ### Arrays
+
+(display-output-format
+ ['[1 2 3 4]
+  (l/! [:code] [1 2 3 4])
+  (l/! [:live] [1 2 3 4])]
+ ['["hello" ["world"]]
+  (l/! [:code] ["hello" ["world"]])
+  (l/! [:live] ["hello" ["world"]])])
+
+;; ### Objects
+
+(display-output-format
+ ['{:a 1 :b 2 :c 3}
+  (l/! [:code] {:a 1 :b 2 :c 3})
+  (l/! [:live] {:a 1 :b 2 :c 3})]
+ ['{:a {:b {:c 3}}}
+  (l/! [:code] {:a {:b {:c 3}}})
+  (l/! [:live] {:a {:b {:c 3}}})])
+
+;; ### Objects - tab
+
+(display-output-format
+ ['(tab ["a" 1] ["b" 2] ["c" 3])
+  (l/! [:code] (tab ["a" 1] ["b" 2] ["c" 3]))
+  (l/! [:live] (tab ["a" 1] ["b" 2] ["c" 3]))])
+
+
+;; # Operations - Assignment
+
+;; ### Var
+
+(display-output-format
+ ['(do (var x 1)
+       x)
+  (l/! [:code]
+    (do (var x 1)
+        x))
+  (l/! [:live]
+    (do (var x 1)
+        x))])
+
+;; ### Const
+
+(display-output-format
+ ['(do (const x 1)
+       x)
+  (l/! [:code]
+    (do (const x 1)
+        x))
+  (l/! [:live]
+    (do (const x 1)
+        x))])
+
+
+;; ### ReAssign
+
+(display-output-format
+ ['(do (var x 1)
+       (:= x 10)
+       x)
+  (l/! [:code]
+    (do (var x 1)
+        (:= x 10)
+        x))
+  (l/! [:live]
+    (do (var x 1)
+        (:= x 10)
+        x))])
+
+
+;; # Operations - Logic
+
+;; ### Negation
+(display-output-format
+ ['(not true)
+  (l/! [:code] (not true))
+  (l/! [:live] (not true))]
+ ['(not false)
+  (l/! [:code] (not false))
+  (l/! [:live] (not false))]
+ ['(not 1)
+  (l/! [:code] (not 1))
+  (l/! [:live] (not 1))]
+ ['(not 0)
+  (l/! [:code] (not 0))
+  (l/! [:live] (not 0))])
+
+;; ### Or
+(display-output-format
+ ['(or 0 false)
+  (l/! [:code] (or 0 false))
+  (l/! [:live] (or 0 false))]
+ ['(or 1 false)
+  (l/! [:code] (or 1 false))
+  (l/! [:live] (or 1 false))]
+ ['(or 0 true)
+  (l/! [:code] (or 0 true))
+  (l/! [:live] (or 0 true))])
+
+;; ### And
+(display-output-format
+ ['(and 0 false)
+  (l/! [:code] (and 0 false))
+  (l/! [:live] (and 0 false))]
+ ['(and 1 false)
+  (l/! [:code] (and 1 false))
+  (l/! [:live] (and 1 false))]
+ ['(and 0 true)
+  (l/! [:code] (and 0 true))
+  (l/! [:live] (and 0 true))])
+
+;; ### Ternary
+(display-output-format
+ ['(:? true 1 2)
+  (l/! [:code] (:? true 1 2))
+  (l/! [:live] (:? true 1 2))]
+ ['(:? (or 0 0) 1 2)
+  (l/! [:code] (:? (or 0 0) 1 2))
+  (l/! [:live] (:? (or 0 0) 1 2))])
 
 
 ;; # Operations - Math
@@ -228,7 +354,6 @@
   (l/! [:code] (- 4 3 2 1.1))
   (l/! [:live] (- 4 3 2 1.1))])
 
-
 ;; ### Multiplication
 
 (display-output-format
@@ -253,6 +378,287 @@
   (l/! [:live] (/ 4 3.3 2.2 1.1))])
 
 
+;; ### Pow
+
+(display-output-format
+ ['(pow 10 2)
+  (l/! [:code] (pow 10 2))
+  (l/! [:live] (pow 10 2))]
+ ['(pow 0.5 0.2)
+  (l/! [:code] (pow 0.5 0.2))
+  (l/! [:live] (pow 0.5 0.2))])
+
+
+;; ### Mod
+
+(display-output-format
+ ['(mod 1123 7)
+  (l/! [:code] (mod 1123 7))
+  (l/! [:live] (mod 1123 7))]
+ ['(mod 1123 7.1)
+  (l/! [:code] (mod 1123 7.1))
+  (l/! [:live] (mod 1123 7.1))])
+
+
+
+;; # Operations - Comparison
+
+;; ### Equals
+
+(display-output-format
+ ['(== 1 1)
+  (l/! [:code] (== 1 1))
+  (l/! [:live] (== 1 1))]
+ ['(== 1 "1")
+  (l/! [:code] (== 1 "1"))
+  (l/! [:live] (== 1 "1"))]
+ ['(== "hello" "hello")
+  (l/! [:code] (== "hello" "hello"))
+  (l/! [:live] (== "hello" "hello"))])
+
+;; ### Triple Equals
+
+(display-output-format
+ ['(=== 1 1)
+  (l/! [:code] (=== 1 1))
+  (l/! [:live] (=== 1 1))]
+ ['(=== 1 "1")
+  (l/! [:code] (=== 1 "1"))
+  (l/! [:live] (=== 1 "1"))]
+ ['(=== "hello" "hello")
+  (l/! [:code] (=== "hello" "hello"))
+  (l/! [:live] (=== "hello" "hello"))])
+
+;; ### Not Equals
+
+(display-output-format
+ ['(not= 1 2)
+  (l/! [:code] (not= 1 2))
+  (l/! [:live] (not= 1 2))]
+ ['(not= 1 "1")
+  (l/! [:code] (not= 1 "1"))
+  (l/! [:live] (not= 1 "1"))]
+ ['(not= "hello" "hello")
+  (l/! [:code] (not= "hello" "hello"))
+  (l/! [:live] (not= "hello" "hello"))])
+
+;; ### Less Than
+
+(display-output-format
+ ['(< 1 2)
+  (l/! [:code] (< 1 2))
+  (l/! [:live] (< 1 2))]
+ ['(< 1 1)
+  (l/! [:code] (< 1 1))
+  (l/! [:live] (< 1 1))]
+ ['(< 1 "2")
+  (l/! [:code] (< 1 "2"))
+  (l/! [:live] (< 1 "2"))])
+
+;; ### Less Than Equals
+
+(display-output-format
+ ['(<= 1 2)
+  (l/! [:code] (<= 1 2))
+  (l/! [:live] (<= 1 2))]
+ ['(<= 1 1)
+  (l/! [:code] (<= 1 1))
+  (l/! [:live] (<= 1 1))]
+ ['(<= 1 "1")
+  (l/! [:code] (<= 1 "1"))
+  (l/! [:live] (<= 1 "1"))])
+
+;; ### Greater Than
+
+(display-output-format
+ ['(> 3 2)
+  (l/! [:code] (> 3 2))
+  (l/! [:live] (> 3 2))]
+ ['(> 3 3)
+  (l/! [:code] (> 3 3))
+  (l/! [:live] (> 3 3))]
+ ['(> 3 "2")
+  (l/! [:code] (> 3 "2"))
+  (l/! [:live] (> 3 "2"))])
+
+;; ### Greater Than Equals
+
+(display-output-format
+ ['(>= 3 2)
+  (l/! [:code] (>= 3 2))
+  (l/! [:live] (>= 3 2))]
+ ['(>= 3 3)
+  (l/! [:code] (>= 3 3))
+  (l/! [:live] (>= 3 3))]
+ ['(>= 3 "3")
+  (l/! [:code] (>= 3 "3"))
+  (l/! [:live] (>= 3 "3"))])
+
+;; # Operations - Counter
+
+;; ### Increment
+
+(display-output-format
+ ['(do (var x 1)
+       (:++ x)
+       x)
+  (l/! [:code]
+    (do (var x 1)
+        (:++ x)
+        x))
+  (l/! [:live]
+    (do (var x 1)
+        (:++ x)
+        x))])
+
+
+;; ### Increment By
+
+(display-output-format
+ ['(do (var x 1)
+       (:+= x 10)
+       x)
+  (l/! [:code]
+    (do (var x 1)
+        (:+= x 10)
+        x))
+  (l/! [:live]
+    (do (var x 1)
+        (:+= x 10)
+        x))])
+
+
+;; ### Decrement
+
+(display-output-format
+ ['(do (var x 5)
+       (:-- x)
+       x)
+  (l/! [:code]
+    (do (var x 5)
+        (:-- x)
+        x))
+  (l/! [:live]
+    (do (var x 5)
+        (:-- x)
+        x))])
+
+;; ### Decrement By
+
+(display-output-format
+ ['(do (var x 5)
+       (:-= x 50)
+       x)
+  (l/! [:code]
+    (do (var x 5)
+        (:-= x 50)
+        x))
+  (l/! [:live]
+    (do (var x 5)
+        (:-= x 50)
+        x))])
+
+
+;; ### Multiply By
+
+(display-output-format
+ ['(do (var x 5)
+       (:*= x 50)
+       x)
+  (l/! [:code]
+    (do (var x 5)
+        (:*= x 50)
+        x))
+  (l/! [:live]
+    (do (var x 5)
+        (:*= x 50)
+        x))])
+
+
+;; # Operations - Bitwise
+
+;; ### Bitwise Or
+
+(display-output-format
+ ['(b:| 7 8)
+  (l/! [:code]
+    (b:| 7 8))
+  (l/! [:live]
+    (b:| 7 8))]
+ ['(b:| 7 7)
+  (l/! [:code]
+    (b:| 7 7))
+  (l/! [:live]
+    (b:| 7 7))]
+ ['(b:| 7 0)
+  (l/! [:code]
+    (b:| 7 0))
+  (l/! [:live]
+    (b:| 7 0))])
+
+;; ### Bitwise And
+
+(display-output-format
+ ['(b:& 7 8)
+  (l/! [:code]
+    (b:& 7 8))
+  (l/! [:live]
+    (b:& 7 8))]
+ ['(b:& 7 7)
+  (l/! [:code]
+    (b:& 7 7))
+  (l/! [:live]
+    (b:& 7 7))]
+ ['(b:& 7 0)
+  (l/! [:code]
+    (b:& 7 0))
+  (l/! [:live]
+    (b:& 7 0))])
+
+;; ### Bitwise Xor
+
+(display-output-format
+ ['(b:xor 7 8)
+  (l/! [:code]
+    (b:xor 7 8))
+  (l/! [:live]
+    (b:xor 7 8))]
+ ['(b:xor 7 7)
+  (l/! [:code]
+    (b:xor 7 7))
+  (l/! [:live]
+    (b:xor 7 7))]
+ ['(b:xor 7 0)
+  (l/! [:code]
+    (b:xor 7 0))
+  (l/! [:live]
+    (b:xor 7 0))])
+
+;; ### Bitshift Right
+
+(display-output-format
+ ['(b:>> 128 3)
+  (l/! [:code]
+    (b:>> 128 3))
+  (l/! [:live]
+    (b:>> 128 3))])
+
+
+;; ### Bitshift Left
+
+(display-output-format
+ ['(b:<< 128 3)
+  (l/! [:code]
+    (b:<< 128 3))
+  (l/! [:live]
+    (b:<< 128 3))])
+
+
+
+;; # Lib - Helpers
+
+;; ### for:array
+
 (display-output-format
  ['(do (var out := [])
        (k/for:array [e [1 2 3 4]]
@@ -274,6 +680,7 @@
             (break))
           (x:arr-push out e))
         out))])
+
 
 
 
