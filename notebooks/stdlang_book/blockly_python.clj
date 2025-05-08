4b6ef339a37dda00b852d9e44887b9f1ea163905
kloimhardt <markus.agwin@gmx.net>
2025-05-08 11:22:10 +0200

    clean

--- notebooks/stdlang_book/blockly_python.clj
+++ notebooks/stdlang_book/blockly_python.clj
@@ -6,16 +6,36 @@
  '[scicloj.kindly.v4.kind :as kind]
  '[scicloj.kindly.v4.api :as kindly])
 
-^:kind/println
+^{:kindly/hide-code true :kind/hidden true}
 (l/script+ [:code :python]
   {:require [[xt.lang.base-lib :as k]
              [xt.lang.base-iter :as it]]})
 
-^:kind/println
+^{:kindly/hide-code true :kind/hidden true}
 (l/script+ [:live :python]
   {:runtime :basic
    :require [[xt.lang.base-lib :as k]
              [xt.lang.base-iter :as it]]})
+
+(kind/scittle
+  '(.log js/console "hello"))
+
+(kind/hiccup
+  [:div
+   [:script {:src "https://kloimhardt.github.io/twotiles/twotiles_core.js"}]
+   [:script "var parse = scittle.core.eval_string(twotiles.parse_clj);"]
+   [:script {:src "https://unpkg.com/blockly/blockly_compressed.js"}]
+   [:script {:src "https://unpkg.com/blockly/msg/en.js"}]
+   [:script "Blockly.defineBlocksWithJsonArray(twotiles.blocks);"]])
+
+(defn tiles-render [code]
+  [:div
+   [:script (str "var xml1 = parse('" code "')")]
+   [:div {:id "blocklyDiv1", :style {:height "180px"}}]
+   [:script "var workspace1 = Blockly.inject('blocklyDiv1',
+{'toolbox': twotiles.toolbox, 'sounds': false})"]
+   [:script "const xmlDom1 = Blockly.utils.xml.textToDom(xml1)"]
+   [:script "Blockly.Xml.clearWorkspaceAndLoadFromXml(xmlDom1,workspace1)"]])
 
 (defn tiles-kw? [e]
   (and (keyword? e) (= (namespace e) "tiles")))
@@ -35,29 +55,6 @@
 (! [:code] (do (def (hello x y) (return (:tiles/infix (+ x y))))
                :tiles/slot
                (hello 1 2)))
-
-^:kindly/hide-code
-(kind/scittle
-  '(.log js/console "hello"))
-
-^:kindly/hide-code
-(kind/hiccup
-  [:div
-   [:script {:src "https://kloimhardt.github.io/twotiles/twotiles_core.js"}]
-   [:script "var parse = scittle.core.eval_string(twotiles.parse_clj);"]
-   [:script {:src "https://unpkg.com/blockly/blockly_compressed.js"}]
-   [:script {:src "https://unpkg.com/blockly/msg/en.js"}]
-   [:script "Blockly.defineBlocksWithJsonArray(twotiles.blocks);"]])
-
-^:kindly/hide-code
-(defn tiles-render [code]
-  [:div
-   [:script (str "var xml1 = parse('" code "')")]
-   [:div {:id "blocklyDiv1", :style {:height "180px"}}]
-   [:script "var workspace1 = Blockly.inject('blocklyDiv1',
-{'toolbox': twotiles.toolbox, 'sounds': false})"]
-   [:script "const xmlDom1 = Blockly.utils.xml.textToDom(xml1)"]
-   [:script "Blockly.Xml.clearWorkspaceAndLoadFromXml(xmlDom1,workspace1)"]])
 
 ^:kind/hiccup
 (tiles-render '(do (def (hello x y) (return (:tiles/infix (+ x y))))
